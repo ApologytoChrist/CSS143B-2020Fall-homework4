259eecd421b6d1f1d809140cb22cbf35fd78c701
Kengli Fu <kenglif12@gmail.com>
2020-11-07 14:15:16 -0800

    Everything Done should refactor SortList

--- src/main/java/Problem2/SortList.java
+++ src/main/java/Problem2/SortList.java
@@ -1,5 +1,7 @@
 package Problem2;
 
+import java.util.List;
+
 public class SortList {
     private static final int SENTRY = Integer.MAX_VALUE;
     // DO NOT ADD ANY NEW MEMBER VARIABLE AND MEMBER FUNCTION
@@ -20,22 +22,72 @@ public class SortList {
         if (head == null || head.next == null) {
             return null;
         }
-        int index = 0;
 
-        findMidHelper(head, index);
+        ListNode x = head;
+        ListNode twoX = head.next;
 
-
-        return null;
-    }
-
-    private static void findMidHelper(ListNode head, int index) {
-        if (head.next != null) {
-            findMidHelper(head.next, index++);
+        while (twoX != null) {
+            twoX = twoX.next;
+            if (twoX != null) {
+                twoX = twoX.next;
+                if (twoX != null) {
+                    x = x.next;
+                }
+            }
         }
+        ListNode secondHead = x.next;
+        x.next = null;
+
+        return secondHead;
     }
+
 
     public static ListNode mergeLists(ListNode list1, ListNode list2) {
         // homework
-        return null;
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        ListNode list1Temp = list1;
+        ListNode list2Temp = list2;
+        ListNode newHead;
+        if (list1Temp == null) {
+            newHead = list2Temp;
+            list2Temp = list2Temp.next;
+        } else if (list2Temp == null) {
+            newHead = list1Temp;
+            list1Temp = list1Temp.next;
+        } else {
+            if (list1Temp.val < list2Temp.val) {
+                newHead = list1Temp;
+                list1Temp = list1Temp.next;
+            } else {
+                newHead = list2Temp;
+                list2Temp = list2.next;
+            }
+        }
+        ListNode temp = newHead;
+
+        while (!(list1Temp == null && list2Temp == null)) {
+            if (list1Temp == null) {
+                temp.next = list2Temp;
+                temp = temp.next;
+                list2Temp = list2Temp.next;
+            } else if (list2Temp == null) {
+                temp.next = list1Temp;
+                temp = temp.next;
+                list1Temp = list1Temp.next;
+            } else {
+                if (list1Temp.val < list2Temp.val) {
+                    temp.next = list1Temp;
+                    temp = temp.next;
+                    list1Temp = list1Temp.next;
+                } else {
+                    temp.next = list2Temp;
+                    temp = temp.next;
+                    list2Temp = list2Temp.next;
+                }
+            }
+        }
+        return newHead;
     }
 }
